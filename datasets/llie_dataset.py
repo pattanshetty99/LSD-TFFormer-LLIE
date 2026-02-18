1bb7c125aee67e74adaaaa1d8b451f6fc493b4f2
pattanshetty99 <pattanshetty99@gmail.com>
2026-02-18 17:16:29 +0530

    Implement LLIE_Dataset class for image loading

---
+++ datasets/llie_dataset.py
@@ -0,0 +1,26 @@
+import os
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+
+class LLIE_Dataset(Dataset):
+    def __init__(self, low_dir, high_dir, img_size=256):
+        self.low_dir = low_dir
+        self.high_dir = high_dir
+        self.files = sorted(os.listdir(low_dir))
+
+        self.transform = transforms.Compose([
+            transforms.Resize((img_size, img_size)),
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        filename = self.files[idx]
+
+        low = Image.open(os.path.join(self.low_dir, filename)).convert("RGB")
+        high = Image.open(os.path.join(self.high_dir, filename)).convert("RGB")
+
+        return self.transform(low), self.transform(high)

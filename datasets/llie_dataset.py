e40ddac3784fe5eb7c89574d6a1bb178e1172bec
pattanshetty99 <pattanshetty99@gmail.com>
2026-02-18 18:05:40 +0530

    Update file filtering in LLIE_Dataset
    Filter files in low_dir to include only image formats.

--- datasets/llie_dataset.py
+++ datasets/llie_dataset.py
@@ -7,7 +7,10 @@ class LLIE_Dataset(Dataset):
     def __init__(self, low_dir, high_dir, img_size=256):
         self.low_dir = low_dir
         self.high_dir = high_dir
-        self.files = sorted(os.listdir(low_dir))
+        self.files = sorted([
+            f for f in os.listdir(low_dir)
+            if f.lower().endswith((".png", ".jpg", ".jpeg"))
+        ])
 
         self.transform = transforms.Compose([
             transforms.Resize((img_size, img_size)),
@@ -24,3 +27,4 @@ class LLIE_Dataset(Dataset):
         high = Image.open(os.path.join(self.high_dir, filename)).convert("RGB")
 
         return self.transform(low), self.transform(high)
+

864ae4fd09360ccda451c111d57bd24dc09c49f9
pattanshetty99 <pattanshetty99@gmail.com>
2026-02-19 12:06:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -8,8 +8,36 @@ from datasets.llie_dataset import LLIE_Dataset
 from models.lsd_tf_former import LSD_TFFormer
 from utils.checkpoint import save_checkpoint, load_checkpoint
 
+import os
+import torch
+
+print("======================================")
+print("Training Script Started")
+print("Process ID (PID):", os.getpid())
+
+print("CUDA Available:", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    print("GPU Name:", torch.cuda.get_device_name(0))
+    print("GPU Count:", torch.cuda.device_count())
+    print("Current CUDA Device:", torch.cuda.current_device())
+else:
+    print("Running on CPU")
+
+print("======================================")
+
+
+print("Starting training script...")
+
+print("TRAIN_LOW:", TRAIN_LOW)
+print("TRAIN_HIGH:", TRAIN_HIGH)
+
 train_dataset = LLIE_Dataset(TRAIN_LOW, TRAIN_HIGH)
+print("Dataset loaded. Number of images:", len(train_dataset))
+
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+print("DataLoader created.")
+
 
 model = LSD_TFFormer().to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
@@ -18,11 +46,17 @@ scaler = GradScaler('cuda')
 
 start_epoch = load_checkpoint(model, optimizer, scaler, CHECKPOINT_PATH, DEVICE)
 
+from tqdm import tqdm
+
 for epoch in range(start_epoch, EPOCHS):
+
     model.train()
     total_loss = 0
 
-    for low, high in train_loader:
+    loop = tqdm(train_loader, desc=f"Epoch [{epoch+1}/{EPOCHS}]")
+
+    for low, high in loop:
+
         low, high = low.to(DEVICE), high.to(DEVICE)
 
         optimizer.zero_grad()
@@ -38,7 +72,8 @@ for epoch in range(start_epoch, EPOCHS):
         scaler.update()
 
         total_loss += loss.item()
+        loop.set_postfix(loss=loss.item())
 
-    print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader):.4f}")
+    print(f"Epoch {epoch+1}, Avg Loss: {total_loss/len(train_loader):.4f}")
 
     save_checkpoint(model, optimizer, scaler, epoch+1, CHECKPOINT_PATH)
